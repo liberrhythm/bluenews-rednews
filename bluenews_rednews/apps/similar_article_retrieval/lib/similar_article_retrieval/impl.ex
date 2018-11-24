26f5082ae636737d151ab4bbed6f30565d2c2826
Sabrina Peng <sabrina.h.peng@gmail.com>
2018-11-24 01:07:28 -0600

    better media bias classification tracking

--- bluenews_rednews/apps/similar_article_retrieval/lib/similar_article_retrieval/impl.ex
+++ bluenews_rednews/apps/similar_article_retrieval/lib/similar_article_retrieval/impl.ex
@@ -1,9 +1,8 @@
 defmodule SimilarArticleRetrieval.Impl do
-
   @base_url "https://newsapi.org/v2"
   @newsapi_key Application.get_env(:similar_article_retrieval, :newsapi_key)
 
-  @default_news_sources %{
+  @news_sources %{
     liberal: %{
       "The New York Times": "the-new-york-times",
       "BBC News": "bbc-news",
@@ -17,12 +16,15 @@ defmodule SimilarArticleRetrieval.Impl do
       "The Hill": "the-hill",
       "National Review": "national-review",
       "The Wall Street Journal": "the-wall-street-journal"
+    },
+    non_partisan: %{
+
     }
   }
 
   def get_all_sources() do
     HTTPoison.get!(@base_url <> "/sources", [], params: [{"apiKey", @newsapi_key}]).body
-      |> Poison.decode!
+    |> Poison.decode!()
   end
 
   def process_keywords(keywords) do
@@ -36,20 +38,67 @@ defmodule SimilarArticleRetrieval.Impl do
       sortBy: "relevancy",
       apiKey: @newsapi_key
     }
+
     URI.encode_query(params)
   end
 
   def config_params(keywords, source) do
-    params = %{
-      q: process_keywords(keywords),
+    %{
+      q: keywords,
       sources: source,
       sortBy: "relevancy",
       apiKey: @newsapi_key
     }
   end
 
+  def is_liberal(src) do
+    Map.has_key?(@news_sources.liberal, src)
+  end
+
+  def is_conservative(src) do
+    Map.has_key?(@news_sources.conservative, src)
+  end
+
+  def is_non_partisan(src) do
+    Map.has_key?(@news_sources.non_partisan, src)
+  end
+
+  def identify_src_bias(src) do
+    cond do
+      is_liberal(src) -> :liberal
+      is_conservative(src) -> :conservative
+      is_non_partisan(src) -> :non_partisan
+      true -> :none
+    end
+  end
+
   def retrieve_article(keywords, source) do
     HTTPoison.get!(@base_url <> "/everything", [], params: config_params(keywords, source)).body
-      |> Poison.decode!
+    |> Poison.decode!()
+  end
+
+  def get_all_articles(keywords) do
+    kw = process_keywords(keywords)
+    get_articles(:liberal, kw)
+    get_articles(:conservative, kw)
+  end
+
+  def get_articles(:liberal, keywords) do
+    retrieve_articles(Map.values(@news_sources.liberal), keywords)
+  end
+
+  def get_articles(:conservative, keywords) do
+    retrieve_articles(Map.values(@news_sources.conservative), keywords)
+  end
+
+  def retrieve_articles(sources, keywords) do
+    sources
+    |> Enum.map(fn src ->
+      Task.async(fn -> SimilarArticleRetrieval.Impl.retrieve_article(keywords, src) end)
+    end)
+    |> Enum.map(fn work ->
+      Task.await(work)
+    end)
+    |> Enum.concat()
   end
 end
